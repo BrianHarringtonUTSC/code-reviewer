69e7ecd23b3361fa18644ffff33f99c421380023
vincent tse <from_vincent@hotmail.com>
2016-06-27 23:38:40 -0400

    added add_comment feature

--- public/javascripts/highlight.js
+++ public/javascripts/highlight.js
@@ -1,24 +1,27 @@
-function highlight() {
+function addCommentFromSelection() {
 	var selection = window.getSelection();
+	if (!selection || selection.isCollapsed) {
+		window.alert('Invalid selection!')
+		return;
+	}
+
 	var anchorNode = selection.anchorNode;
 	var focusNode = selection.focusNode;
-	window.alert(anchorNode);
-	window.alert(anchorNode);
 
-	while (anchorNode.nodeName != "LI") {
+	while (anchorNode.nodeName != 'LI') {
 		if (anchorNode.parentNode != null) {
 			anchorNode = anchorNode.parentNode;
 		} else {
-			window.alert("Invalid selection!")
+			window.alert('Invalid selection!')
 			return;
 		}
 	}
 
-	while (focusNode.nodeName != "LI") {
+	while (focusNode.nodeName != 'LI') {
 		if (focusNode.parentNode != null) {
 			focusNode = focusNode.parentNode;
 		} else {
-			window.alert("Invalid selection!")
+			window.alert('Invalid selection!')
 			return;
 		}
 	}
@@ -30,8 +33,18 @@ function highlight() {
 		endLine = [startLine, startLine = endLine][0];
 	}
 
+	var comment = window.prompt('Please enter your comment:');
+
+	if (comment) {
+		highlight(startLine, endLine, comment);
+	}
+
+	window.getSelection().removeAllRanges();
+}
+
+function highlight(startLine, endLine, comment) {
 	for (var i = startLine; i <= endLine; i++) {
-		nextElement = document.getElementById("line-" + String(i));
+		nextElement = document.getElementById('line-' + String(i));
 		if (nextElement.className === "highlight-1") {
 			nextElement.className = "highlight-2";
 		} else if (nextElement.className === "highlight-2") {
@@ -41,7 +54,23 @@ function highlight() {
 		} else {
 			nextElement.className = "highlight-1";
 		}
-	};
+		nextElement.addEventListener("mouseover", showPopup.bind(null, i));
+		nextElement.addEventListener("mouseout", hidePopup);
 
-	document.body.deselectAll();
+		nextElement.setAttribute('comment', (nextElement.getAttribute('comment') ? nextElement.getAttribute('comment') : '') + comment + '<br>');
+	};
+}
+
+function showPopup(line) {
+	var x = event.clientX;
+	var y = event.clientY;
+	var element = document.getElementById('popup');
+	element.style.display = 'block';
+	element.style.top = y;
+	element.style.left = x;
+	document.getElementById('popup').innerHTML = document.getElementById('line-' + String(line)).getAttribute('comment');
+}
+
+function hidePopup() {
+	document.getElementById('popup').style.display = 'none';
 }
