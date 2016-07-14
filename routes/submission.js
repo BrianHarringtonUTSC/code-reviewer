08a8944c981c30890385f0d5a45032d598e4b457
Bo Zhao <zb.ca@foxmail.com>
2016-07-13 23:53:37 -0400

    minor change in submisison and home page

--- routes/submission.js
+++ routes/submission.js
@@ -20,23 +20,17 @@ router.get('/', function(req, res, next) {
 	  	console.log("this student is not registered in this class");
 	  	res.redirect('/');
 	  } else { // this student is registered in this class
-	  	console.log("req.session is ");
-	  	console.log(req.session['submission_name']);
-	  	return;
-	  	//var submission_name = req.flash('submission_name');
 	  	// redirect to home page if no submission name is specified
-	  	if (submission_name.length == 0) {
+	  	if (req.session.submission_name == null) {
 	  		console.log("please specify a submission name");
 	  		res.redirect('/home');
 	  	} else {
-	  		// pop out submission name from length 1 array
-	  		var submission_name = submission_name.pop();
-	  		console.log("the submission name is " + submission_name);
+	  		console.log("the submission name is " + req.session.submission_name);
 	  		// write submission name to flash message for future use
 	  		//req.flash('submission_name', submission_name);
 	  		res.render('submission', {
 	  			title : 'submission',
-	  			submission_name : submission_name
+	  			submission_name : req.session.submission_name
 	  		});
 	  	}
 	  }
@@ -45,13 +39,11 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/go_to_instruction', function(req, res, next) {
-	req.flash('submission_name', submission_name);
 	res.redirect('/instruction');
 });
 
 
 router.post('/go_to_self_review', function(req, res, next) {
-	req.flash('submission_name', submission_name);
 	res.redirect('/self_review');
 });
 
