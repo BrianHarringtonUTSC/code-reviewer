029add3089fc38d60be729ff8a81bfcc9b734502
Vincent Tse <from_vincent@hotmail.com>
2016-07-13 23:28:10 -0400

    change self review using session

--- routes/self_review.js
+++ routes/self_review.js
@@ -13,20 +13,6 @@ var code_model = mongoose.model(work_name, code_schema);
 var review_model = mongoose.model('a2_reviews', review_schema);
 var student_model = require('./models/student_model.js');
 
-
-var num_of_peers = 10;
-var peer_number = 1;
-var review_array = [];
-var feedbacks = [];
-var num_of_stars = [];
-var highlight_str = "";
-//var self_utorid = "luijerr1";
-//var self_utorid = "lossevki";
-var self_utorid = "";
-var code_path = "";
-var feedback_questions = [];
-var first_time = 1;
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	// user authentication
@@ -39,11 +25,10 @@ router.get('/', function(req, res, next) {
 	  if (student == null) {
 	  	res.redirect('/instructor')
 	  } else {
-	  	if (first_time) {
-	  		first_time = 0;
-	  		get_feedback_questions(req, res, 'peer_review');
+	  	if (req.session.self_utorid == null) {
+	  		init_all(req, res, 'self_review');
 	  	} else {
-	  		get_student_utorid(req, res, 'peer_review');
+	  		get_student_utorid(req, res, 'self_review');
 	  	}
 	  }
 	 });
@@ -51,20 +36,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/go_to_self_review', function(req, res, next) {
-	console.log(req.body);
-	for (var i = 1; i <= num_of_peers; i++) {
+	for (var i = 1; i <= req.session.review_array.length; i++) {
 	  var key = "peer_" + String(i);
 	  if (key in req.body) {
-	    peer_number = i;
+	    req.session.peer_number = i;
 	  }
 	}
 	res.redirect('/self_review');
 });
 
+function init_all(req, res, site) {
+	req.session.self_utorid = '';
+	req.session.review_array = [];
+	req.session.feedbacks = [];
+  	req.session.highlight_str = '';
+  	req.session.peer_number = 1;
+  	req.session.num_stars = 0;
+  	req.session.code_path = '';
+  	req.session.feedback_questions = [];
+  	get_feedback_questions(req, res, site);
+
+}
 function get_feedback_questions(req, res, site) {
 	rule_model.findOne({ work_name: work_name }, function (err, rule) {
 	  if (err) return err;
-	  feedback_questions = rule.feedback_questions;
+	  req.session.feedback_questions = rule.feedback_questions;
 	  get_student_utorid(req, res, site);
 	 });
 }
@@ -72,25 +68,25 @@ function get_feedback_questions(req, res, site) {
 function get_student_utorid(req, res, site) {
 	student_model.findOne({ email: req.user.emails[0].value }, function (err, student) {
 	  if (err) return err;
-	  self_utorid = student.utorid;
-	  find_student_code(res, site);
+	  req.session.self_utorid = student.utorid;
+	  find_student_code(req, res, site);
 	 });
 }
 
-function find_student_code(res, site) {
-  code_model.findOne({ utorid: self_utorid }, function(err, code) {
-  	review_array = code.review_by;
-  	code_path = code.code_path;
-  	find_feedback(res, site);
+function find_student_code(req, res, site) {
+  code_model.findOne({ utorid: req.session.self_utorid }, function(err, code) {
+  	req.session.review_array = code.review_by;
+  	req.session.code_path = code.code_path;
+  	find_feedback(req, res, site);
   });
 }
 
-function find_feedback(res, site) {
-  review_model.findOne({ author: self_utorid, review_by:review_array[peer_number-1] }, function(err, review) {
-	  feedbacks= review.feedbacks;
-	  num_of_stars = review.num_stars;
-	  highlight_str = review.highlights;
-	  read_file(res, site);
+function find_feedback(req, res, site) {
+  review_model.findOne({ author: req.session.self_utorid, review_by: req.session.review_array[req.session.peer_number-1] }, function(err, review) {
+	  req.session.feedbacks= review.feedbacks;
+	  req.session.num_stars = review.num_stars;
+	  req.session.highlight_str = review.highlights;
+	  read_file(req, res, site);
   });
   
 }
@@ -98,8 +94,8 @@ function find_feedback(res, site) {
 var readline = require('readline');
 var stream = require('stream');
 
-function read_file(res, site) {
-	var instream = fs.createReadStream(code_path);
+function read_file(req, res, site) {
+	var instream = fs.createReadStream(req.session.code_path);
 	var outstream = new stream;
 	var rl = readline.createInterface(instream, outstream);
 	var str = ""
@@ -111,13 +107,13 @@ function read_file(res, site) {
 	rl.on('close', function() {
 		res.render(site, {
 			title: site,
-			entries: review_array,
+			entries: req.session.review_array,
 			code: str,
-			peer_num: peer_number,
-			feedbacks: feedbacks,
-			number_of_stars: num_of_stars,
-			init_highligts: highlight_str,
-			feedback_questions: feedback_questions
+			peer_num: req.session.peer_number,
+			feedbacks: req.session.feedbacks,
+			num_stars: req.session.num_stars,
+			init_highligts: req.session.highlight_str,
+			feedback_questions: req.session.feedback_questions
 		});
 	  // do something on finish here
 	  //console.log(str);
